651dde34e936ad7230b15e00b63a4d5658309f97
Andrij Matviiv <andrey777matvyiv@gmail.com>
2023-08-16 23:58:41 +0300

    final

---
+++ TrackBot.Infrastructure/Migrations/20230816205715_AddTableUserLanguagePreference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackBot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableUserLanguagePreference : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLanguagePreference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    language_code = table.Column<int>(type: "int", unicode: false, maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLanguagePreference", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserLanguagePreference");
+        }
+    }
+}
